ddbe51421fadeba81ad65dd61b5f15fc83ea5bac
Sambhav782 <sambhav782@gmail.com>
2024-02-03 21:26:56 +0530

    navbar added

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,19 +1,29 @@
-import React from "react";
-import { UilInfo } from "@iconscout/react-unicons";
-import { UilEstate } from "@iconscout/react-unicons";
+import React, { useState } from "react";
+import { UilAngleDown } from "@iconscout/react-unicons";
 import "./navbar.css";
-
-function Navbar() {
+export default function Navbar() {
+  const [drop, setDrop] = useState(true);
   return (
-    <>
+    <div>
       <div className="navBody">
-        <div className="navName">digiQueen</div>
-        <div className="navObj"></div>
-        <UilInfo />
-        <UilEstate />
+        <div className="navBanner">DigiQueen</div>
+        <div className="navOptions">
+          <div className="homeTab">Home</div>
+          <div className="aboutTab">About</div>
+          <div className="serviceTab">
+            <div className="serviceButton" onClick={() => setDrop(!drop)}>
+              Service <UilAngleDown />
+            </div>
+            <ul className={drop ? "serviceListOffline" : "serviceList"}>
+              <li>Digital Marketing</li>
+              <li>Video Production</li>
+              <li>Graphic Design</li>
+              <li>SEO</li>
+            </ul>
+          </div>
+          <div className="contactTab">Contact</div>
+        </div>
       </div>
-    </>
+    </div>
   );
 }
-
-export default Navbar;
